d399fb2978159b6e5e6aae62eae91d25da50ac89
Ole Petersen <ole.petersen@tum.de>
2024-05-07 11:05:39 +0200

    Implement working memory leak

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import gymnasium as gym
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+import torch
 
 # Parallel environments
-vec_env = make_vec_env("CartPole-v1", n_envs=100)
+train=True
+vec_env = make_vec_env("CartPole-v1", n_envs=2)
+if train:
 
-model = PPO("MlpPolicy", vec_env, verbose=1, device="cpu")
-model.learn(total_timesteps=1)
-model.save("ppo_cartpole")
+    model = PPO("MlpPolicy", vec_env, verbose=1, device="cuda:2")
+    model.learn(total_timesteps=1)
+    model.ep_info_buffer.extend([torch.ones(10000,device="cuda:2")])
+    model.save("ppo_cartpole")
 
-del model # remove to demonstrate saving and loading
+    del model
 
-model = PPO("MlpPolicy", vec_env, verbose=1, device="cpu")
-import resource
-for i in range(10):
-    model.set_parameters("ppo_cartpole.zip")
-    print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+model = PPO("MlpPolicy", vec_env, verbose=1, device="cuda:3")
+import torch
+model.set_parameters("ppo_cartpole.zip", device="cuda:3")
+torch.cuda.empty_cache()
+import time
+print("watch gpustat now")
+time.sleep(10)
+print("memory on ep info device:", torch.cuda.memory_allocated("cuda:2"))
